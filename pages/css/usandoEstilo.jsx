41ee6fb3946a9b6c8e9ca501b75a5959a4762cfa
EnriqueSFlor <enriquee.flor@gmail.com>
2022-01-12 19:34:59 -0300

    .

---
+++ pages/css/usandoEstilo.jsx
@@ -0,0 +1,14 @@
+import Estilo from "../../components/Estilo";
+
+export default function usandoEstilo (){
+    return (
+        <>
+            <Estilo numero = {10} 
+            color="#000"
+            direita/>
+            <Estilo numero = {-3}
+             color="#fff"
+             />
+        </>
+    )
+}
